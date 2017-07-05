214b8d466c1da07225bb2d49687faa098150e714
m0x72 <m0x72@users.noreply.github.com>
2017-07-05 21:17:25 +0200

    plot update

--- pycode/functions.py
+++ pycode/functions.py
@@ -1,16 +1,16 @@
-from math import log10
+from math import log10, log2, cos, sin
 from parameters import *
 
 # Free Space Path Loss (1m reference)
-def fspl_1m(freq):
-    return 32.4 + 20 * log10(freq) # def. 28 GHz
+def free_space_path_loss_1m(carrier_freq):
+    return 32.4 + 20 * log10(carrier_freq) # def. 28 GHz
 
 
 # Log Normal Shadowing Path Loss
 #n: Path loss exponent (1 < n < 5)
 #chi: standard variation fadin (0 < chi < 20)
-def lognormal_pl(f, d, n, chi):
-    return fspl_1m(f) + 10 * n * log10(d) + chi
+def lognormal_path_loss(carrier_freq, tr_distance, pl_exponent, chi = 4):
+    return free_space_path_loss_1m(carrier_freq) + 10 * pl_exponent * log10(tr_distance) + chi
 
 
 # Weather attenuation factor
@@ -21,19 +21,58 @@ AT = 0
 
 
 #d: # foilage depth (max 400m)
-def fl(f, d):
-    if 14 < d <= 400:
-        return 1.33 * (f ** 0.284) * (d ** 0.588)
-    elif 0 < d <= 14:
-        return 0.45 * (f ** 0.284) * d
+def foilage_loss(carrier_freq, foilage_depth):
+    if 14 < foilage_depth <= 400:
+        return 1.33 * (carrier_freq ** 0.284) * (foilage_depth ** 0.588)
+    elif 0 < foilage_depth <= 14:
+        return 0.45 * (carrier_freq ** 0.284) * foilage_depth
     else:
         print("Input distance not in range 0 to 400.")
 
 
-# Total Path loss:
-def pl(f, d):
-    return lognormal_pl(f, d, n = 1, chi = 0) + AT + fl(f, d)
+#The MED model is found to be applicable to cases in which the ray path is
+#blocked by dense, dry, in-leaf trees found in temperate-latitude forests
+def rain_loss(rain):
+    theta = 0; # 0 deg elevation
+    tau = 0; # horizontal polarization
+    kh = 0.2051; # 28Ghz coefficients
+    ah = 0.9679;
+    kv = 0.1964;
+    av = 0.9277;
+    k = (kh + kv + (kh - kv) * cos(theta)**2 * cos(2 * tau) ) / 2;
+    a = (kh * ah + kv * av + ( kh * ah - kv * av) * cos(theta) **2 * cos(2 * tau) ) / 2 * k;
+    return (k * rain ** a) / 1000; #return in db/m
 
+
+# Total Path loss:
+def path_loss(tr_distance, carrier_freq, pl_exponent):
+    return lognormal_path_loss(carrier_freq, tr_distance, pl_exponent, chi = 0); # + AT + fl(carrier_freq, foilage_depth = 10)
+
+
+def friis(pathloss, tx_power, tx_gain, rx_gain):
+    res = tx_power + tx_gain + rx_gain - pathloss;
+    #print(pathloss, tx_power, tx_gain, rx_gain, res); 
+    return res;
+
+def nyquist_noise(bandwidth, temp = 20):
+    kb = 1.38065e-23; #Boltzmann Constant
+    temp_kelvin = 273.15 + temp;
+    return 10 * log10(kb * temp_kelvin * 1e3) + 10 * log10(bandwidth)
+
+def snr_db(signal, noise):
+    res = signal + noise;
+    print(signal, noise, res);
+    return res;
+
+def snr(signal, noise):
+    # convert dbm to power (watts)
+    s = 10**((signal-30)/10);
+    n = 10**((noise-30)/10);
+    return s/n;
+    #return signal - noise;
+
+def shannon_capacity(bandwidth, snr):
+    return bandwidth * log2(1 + snr);
 
 labels =   ["Power in dB", "Distance in m", "Freq. Bandwidth in Hz", "Environment", "Vegetation [1 . . 0]", "Topology [1 . . 0]", "Weather attenuation factor [1 . . 0]"]
 
--- pycode/model.py
+++ pycode/model.py
@@ -5,7 +5,8 @@ from tkinter import messagebox
 from functions import *
 from parameters import *
 #from mimo import *
-#import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
+import numpy as np
 
 #TODO add carrier frequency in GHz to parameters
 #TODO: Complete 'snr_call' func parameters
@@ -22,12 +23,113 @@ from parameters import *
 
 # power = 100.00, distance = 50, freq_band = 200, vegetation = 0, buildings = 0, weatherAF = 0
 def results_func():
-    message = "Norm PL:\t" + str(round(lognormal_pl(f, d, 1, 1), round_to_n)) + " Units Log" '\n' +\
-              "FL:\t" + str(round(fl(f, d), round_to_n)) + " Units" + '\n' +\
-              "PL:\t" + str(round(pl(f, d), round_to_n)) + " Units"
+    message = "Norm PL:\t" + str(round(lognormal_path_loss(f, d, 1, 1), round_to_n)) + " Units Log" '\n' +\
+              "FL:\t" + str(round(foilage_loss(f, d), round_to_n)) + " Units" + '\n' +\
+              "PL:\t" + str(round(path_loss(f, d), round_to_n)) + " Units"
     messagebox.showinfo("Results", message)
 
 
+def plot_func():
+    d1 = np.arange(10, 400, 10) #start 0, end 400, step 10
+
+    plt.figure(1)
+    plt.subplot(211)
+    plt.ylabel('Path Loss [dB]');
+    plt.xlabel('distance [m]');
+    print(d1);
+
+    # Log Normal Path Loss
+    dat1 = [];
+    for d in d1:
+        dat1.append( path_loss(d, 28, 2) );
+
+    #plt.plot(d1, dat1, 'r.', label='LOS'); 
+
+    # Log Normal Path Loss NLOS
+    dat18 = [];
+    for d in d1:
+        dat18.append( path_loss(d, 28, 3.2) );
+
+    plt.plot(d1, dat18, 'b.', label='NLOS'); 
+
+    # Log Normal + Foilage Path Loss
+    dat2 = [];
+    for d in d1:
+        dat2.append( path_loss(d, 28, 3.2) + foilage_loss(28, 10) );
+
+    plt.plot(d1, dat2, 'g.', label='NLOS + 10m foliage');
+
+    # Log Normal + Extreme Rain Loss 50mm/h 
+    dat3 = [];
+    for d in d1:
+        dat3.append( path_loss(d, 28, 3.2) + rain_loss(50) );
+
+    plt.plot(d1, dat3, 'y+', label='NLOS + 50mm/h rain');
+
+
+    plt.legend(loc='upper right');
+
+
+    #plt.subplot(212)
+    #plt.ylabel('snr');
+
+    dat10 = [];
+    for d in d1:
+        dat10.append(
+                snr_db(
+                    friis(path_loss(d, 28, 2), 30, 10, 10),
+                    nyquist_noise(200e6)
+                    )
+                );
+    #plt.plot(d1, dat10, 'r.', label='LOS');
+
+    dat11 = [];
+    for d in d1:
+        dat11.append(
+                snr_db(
+                    friis(path_loss(d, 28, 2), 30, 10, 10) + foilage_loss(28, 10),
+                    nyquist_noise(200e6)
+                    )
+                );
+    #plt.plot(d1, dat11, 'g.', label='LOS');
+
+
+
+
+    plt.subplot(212)
+    plt.ylabel('Channel Capacity [b/s]');
+    plt.xlabel('distance [m]');
+
+    # Channel Capacity over Distance
+    dat3 = [];
+    for d in d1:
+        dat3.append( 
+            shannon_capacity(200e6, 
+                snr( 
+                    friis(path_loss(d, 28, 2) + 0, 10, 0, 0), 
+                    nyquist_noise(200e6)
+                    )
+                ) 
+            );
+
+    #plt.plot(d1, dat3, 'r.', label='LOS');
+
+    # Channel Capacity over Distance
+    dat4 = [];
+    for d in d1:
+        dat4.append( 
+            shannon_capacity(200e6, 
+                snr( 
+                    friis(path_loss(d, 28, 3.2) + foilage_loss(28, 10), 10, 0, 0), 
+                    nyquist_noise(200e6)
+                    )
+                ) 
+            );
+
+    plt.plot(d1, dat4, 'g.', label='NLOS + 10m foliage');
+    plt.legend(loc='upper right');
+    
+    plt.show()
 
 
 
@@ -46,6 +148,9 @@ for i, label in enumerate(labels):
 
 
 button = Button(master, text="Simulate", command = results_func).grid(row = len(labels), column = 1)
+button2 = Button(master, text="Plot", command = plot_func).grid(row = len(labels)+1, column = 1)
+
+
 
 
 mainloop()
